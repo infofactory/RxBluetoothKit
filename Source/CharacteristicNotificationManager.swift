ac466a3cf3aae694220cde1f01b040fd19311bdc
Massimiliano Valotto <valotto@infofactory.it>
2022-07-20 14:30:27 +0200

    Fix notifica su characteristic presente su più servizi.

--- Source/CharacteristicNotificationManager.swift
+++ Source/CharacteristicNotificationManager.swift
@@ -6,8 +6,8 @@ class CharacteristicNotificationManager {
 
     private unowned let peripheral: CBPeripheral
     private let delegateWrapper: CBPeripheralDelegateWrapper
-    private var uuidToActiveObservableMap: [CBUUID: Observable<Characteristic>] = [:]
-    private var uuidToActiveObservablesCountMap: [CBUUID: Int] = [:]
+    private var uuidToActiveObservableMap: [CBUUID: [CBUUID: Observable<Characteristic>]] = [:]
+    private var uuidToActiveObservablesCountMap: [CBUUID: [CBUUID: Int]] = [:]
     private let lock = NSLock()
 
     init(peripheral: CBPeripheral, delegateWrapper: CBPeripheralDelegateWrapper) {
@@ -20,7 +20,7 @@ class CharacteristicNotificationManager {
             guard let strongSelf = self else { throw BluetoothError.destroyed }
             strongSelf.lock.lock(); defer { strongSelf.lock.unlock()}
 
-            if let activeObservable = strongSelf.uuidToActiveObservableMap[characteristic.uuid] {
+            if let activeObservable = strongSelf.uuidToActiveObservableMap[characteristic.service.uuid]?[characteristic.uuid] {
                 return activeObservable
             }
 
@@ -29,23 +29,36 @@ class CharacteristicNotificationManager {
                 .do(onSubscribed: { [weak self] in
                     guard let strongSelf = self else { return }
                     strongSelf.lock.lock(); defer { strongSelf.lock.unlock() }
-                    let counter = strongSelf.uuidToActiveObservablesCountMap[characteristic.uuid] ?? 0
-                    strongSelf.uuidToActiveObservablesCountMap[characteristic.uuid] = counter + 1
+                    let counter = strongSelf.uuidToActiveObservablesCountMap[characteristic.service.uuid]?[characteristic.uuid] ?? 0
+                    if strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] == nil {
+                        strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] = [:]
+                    }
+                    strongSelf.uuidToActiveObservablesCountMap[characteristic.service.uuid]?[characteristic.uuid] = counter + 1
                     self?.setNotifyValue(true, for: characteristic)
                 }, onDispose: { [weak self] in
                     guard let strongSelf = self else { return }
                     strongSelf.lock.lock(); defer { strongSelf.lock.unlock() }
-                    let counter = strongSelf.uuidToActiveObservablesCountMap[characteristic.uuid] ?? 1
-                    strongSelf.uuidToActiveObservablesCountMap[characteristic.uuid] = counter - 1
+                    let counter = strongSelf.uuidToActiveObservablesCountMap[characteristic.service.uuid]?[characteristic.uuid] ?? 1
+                    if strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] == nil {
+                        strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] = [:]
+                    }
+                    strongSelf.uuidToActiveObservablesCountMap[characteristic.service.uuid]?[characteristic.uuid] = counter - 1
 
                     if counter <= 1 {
-                        strongSelf.uuidToActiveObservableMap.removeValue(forKey: characteristic.uuid)
+                        strongSelf.uuidToActiveObservableMap[characteristic.service.uuid]?.removeValue(forKey: characteristic.uuid)
+                        if strongSelf.uuidToActiveObservableMap[characteristic.service.uuid]?.count ?? 0 > 0 {
+                            strongSelf.uuidToActiveObservableMap.removeValue(forKey: characteristic.service.uuid)
+                        }
                         strongSelf.setNotifyValue(false, for: characteristic)
                     }
                 })
                 .share()
 
-            strongSelf.uuidToActiveObservableMap[characteristic.uuid] = observable
+            if strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] == nil {
+                strongSelf.uuidToActiveObservableMap[characteristic.service.uuid] = [:]
+            }
+            strongSelf.uuidToActiveObservableMap[characteristic.service.uuid]?[characteristic.uuid] = observable
+            
             return observable
         }
     }
